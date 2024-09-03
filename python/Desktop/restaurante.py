8686a61baca19666c28b7a2509a02edb044e2a49
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-03 01:05:48 -0300

    oi
    oi

--- python/Desktop/restaurante.py
+++ python/Desktop/restaurante.py
@@ -14,7 +14,13 @@ def tela():
     tela1.tkraise()
 
 def telatwo():
-    tela.tkraise()
+    telat2.tkraise()
+
+def telathree():
+    telat3.tkraise()
+
+def telafour():
+    telat4.tkraise()
 
 def validarLogin():
     usuario = usuarioentry.get()
@@ -97,7 +103,7 @@ botalcol = Image.open("imagens\\botbedalcolicas.png")
 botalcol1 = ImageTk.PhotoImage(botalcol)
 balcol = tk.Label(tPrincipal, image=botalcol1, bd=0)
 balcol.place(rely=0.32, relx=0.5, anchor='n')
-balcol.bind('<Button-1>', lambda e: print('Opa'))
+balcol.bind('<Button-1>', lambda e: telathree())
 
 
 ## sobremesas
@@ -258,13 +264,13 @@ botaosucolaranja.bind('<Button-1>', lambda e: print('Opa'))
 
 
 
-tela = tk.Frame(root, background='yellow')
+telat2 = tk.Frame(root, background='yellow')
 
 # tPrincipal = tk.Frame(root, background='#ffff12')
 
-tela.grid(column=0, row=0, sticky='nsew')
+telat2.grid(column=0, row=0, sticky='nsew')
 
-titulotela = tk.Label(tela, text="Restaurante do Ederson", font=("Arial", 30, "bold"), anchor="center", background="yellow")
+titulotela = tk.Label(telat2, text="Restaurante do Ederson", font=("Arial", 30, "bold"), anchor="center", background="yellow")
 titulotela.place(rely=0.00, relx=0.5, anchor='n')
 
 ## PÃO AUSTRALIANO ==              ==                ==                 ==               == 
@@ -273,28 +279,167 @@ titulotela.place(rely=0.00, relx=0.5, anchor='n')
 pao = Image.open("imagens\\bebidas\\entradas\\paoutback.png")
 pao.thumbnail((300, 300))
 fot1 = ImageTk.PhotoImage(pao)
-label_pao = tk.Label(tela, image=fot1, bg="gray", bd=0)
+label_pao = tk.Label(telat2, image=fot1, bg="gray", bd=0)
 label_pao.place(rely=0.3, relx=0.75, anchor='s')
 
 botaopao = Image.open("imagens\\bebidas\\entradas\\botpao.png")
 botaopao1 = ImageTk.PhotoImage(botaopao)
-botaopao = tk.Label(tela, image=botaopao1, bd=0)
+botaopao = tk.Label(telat2, image=botaopao1, bd=0)
 botaopao.place(rely=0.32, relx=0.75, anchor='n')
 botaopao.bind('<Button-1>', lambda e: print('Opa'))
 
-## ALHEIRA
+# # ALHEIRA
 
 alheira = Image.open("imagens\\bebidas\\entradas\\alheira.png")
 alheira.thumbnail((300, 300))
 fotoalheira = ImageTk.PhotoImage(alheira)
-label_alheira = tk.Label(tela, image=fotoalheira, bg="gray", bd=0)
+label_alheira = tk.Label(telat2, image=fotoalheira, bg="gray", bd=0)
 label_alheira.place(rely=0.3, relx=0.5, anchor='s')
 
 botaoalheira = Image.open("imagens\\bebidas\\entradas\\botalheira.png")
 botaoalheira1 = ImageTk.PhotoImage(botaoalheira)
-botaoalheira = tk.Label(tela, image=botaoalheira1, bd=0)
+botaoalheira = tk.Label(telat2, image=botaoalheira1, bd=0)
 botaoalheira.place(rely=0.40, relx=0.5, anchor='s')
 botaoalheira.bind('<Button-1>', lambda e: print('Opa'))
+
+
+## quibebe ==             ==                      ==                         ==
+
+
+quibebe = Image.open("imagens\\bebidas\\entradas\\quibebe.png")
+quibebe.thumbnail((300, 300))
+fotoquibebe = ImageTk.PhotoImage(quibebe)
+label_quibebe = tk.Label(telat2, image=fotoquibebe, bg="gray", bd=0)
+label_quibebe.place(rely=0.3, relx=0.25, anchor='s')
+
+botaoquibebe = Image.open("imagens\\bebidas\\entradas\\botquibebe.png")
+botaoquibebe1 = ImageTk.PhotoImage(botaoquibebe)
+botaoquibebe = tk.Label(telat2, image=botaoquibebe1, bd=0)
+botaoquibebe.place(rely=0.40, relx=0.25, anchor='s')
+botaoquibebe.bind('<Button-1>', lambda e: print('Opa'))
+
+
+
+
+antepasto = Image.open("imagens\\bebidas\\entradas\\antepasto.png")
+antepasto.thumbnail((300, 300))
+fotoantepasto = ImageTk.PhotoImage(antepasto)
+labelantepasto = tk.Label(telat2, image=fotoantepasto, bg="gray", bd=0)
+labelantepasto.place(rely=0.52, relx=0.35, anchor='n')
+
+botaoantepasto = Image.open("imagens\\bebidas\\entradas\\botantepasto.png")
+botaoantepasto1 = ImageTk.PhotoImage(botaoantepasto)
+botaoantepasto = tk.Label(telat2, image=botaoantepasto1, bd=0)
+botaoantepasto.place(rely=0.81, relx=0.35, anchor="s")
+botaoantepasto.bind('<Button-1>', lambda e: print('Opa'))
+
+
+
+
+bololegumes = Image.open("imagens\\bebidas\\entradas\\bololegumes.png")
+bololegumes.thumbnail((300, 300))
+bololegumes1 = ImageTk.PhotoImage(bololegumes)
+labelbololegumes = tk.Label(telat2, image=bololegumes1, bg="gray", bd=0)
+labelbololegumes.place(rely=0.52, relx=0.65, anchor='n')
+
+botaobololegumes = Image.open("imagens\\bebidas\\entradas\\botbololegumes.png")
+botaobololegumes1 = ImageTk.PhotoImage(botaobololegumes)
+botaobololegumes = tk.Label(telat2, image=botaobololegumes1, bd=0)
+botaobololegumes.place(rely=0.81, relx=0.65, anchor="s")
+botaobololegumes.bind('<Button-1>', lambda e: print('Opa'))
+
+## tela3 ==           ==                  ==                  ==                ==                     ==
+
+telat3 = tk.Frame(root, background='#ffff12')
+telat3.grid(column=0, row=0, sticky='nsew')
+
+titulot3 = tk.Label(telat3, text="Restaurante do Ederson", font=("Arial", 30, "bold"), anchor="center", background="yellow")
+titulot3.place(rely=0.00, relx=0.5, anchor='n')
+
+## gold label =          ==                  ==              ==              ==
+
+goldlabel = Image.open("bebidalcolica\\goldlabel2.png")
+goldlabel.thumbnail((300, 300))
+goldlabel1 = ImageTk.PhotoImage(goldlabel)
+labelgoldlabel = tk.Label(telat3, image=goldlabel1, bg="gray", bd=0)
+labelgoldlabel.place(rely=0.4, relx=0.75, anchor='s')
+
+botaogoldlabel = Image.open("bebidalcolica\\botaogoldlabel.png")
+botaogoldlabel1 = ImageTk.PhotoImage(botaogoldlabel)
+botaogoldlabel = tk.Label(telat3, image=botaogoldlabel1, bd=0)
+botaogoldlabel.place(rely=0.42, relx=0.75, anchor="n")
+botaogoldlabel.bind('<Button-1>', lambda e: print('Opa'))
+
+
+## jack daniels ==             ==                        ==         ==
+
+
+jackdaniels = Image.open("bebidalcolica\\jackdanielss.png")
+jackdaniels.thumbnail((300, 300))
+jackdaniels1 = ImageTk.PhotoImage(jackdaniels)
+label_jackdaniels = tk.Label(telat3, image=jackdaniels1, bg="gray", bd=0)
+label_jackdaniels.place(rely=0.4, relx=0.5, anchor='s')
+
+botaojackdaniels = Image.open("bebidalcolica\\botaojackdenielss.png")
+botaojackdaniels1 = ImageTk.PhotoImage(botaojackdaniels)
+botaojackdaniels = tk.Label(telat3, image=botaojackdaniels1, bd=0)
+botaojackdaniels.place(rely=0.50, relx=0.5, anchor='s')
+botaojackdaniels.bind('<Button-1>', lambda e: print('Opa'))
+
+## royal salute ==           ==                ==         == 
+
+
+
+royalsalute = Image.open("bebidalcolica\\royalsalutee.png")
+royalsalute.thumbnail((300, 300))
+fotoroyalsalute = ImageTk.PhotoImage(royalsalute)
+label_royalsalute = tk.Label(telat3, image=fotoroyalsalute, bg="gray", bd=0)
+label_royalsalute.place(rely=0.4, relx=0.25, anchor='s')
+
+# botaoroyalsalute = Image.open("bebidalcolica\\botaoroyasalute.png")
+# botaoroyalsalute1 = ImageTk.PhotoImage(botaoroyalsalute)
+# botaoquibebe = tk.Label(telat3, image=botaoroyalsalute1, bd=0)
+# botaoroyalsalute.place(rely=0.20, relx=0.25, anchor='s')
+# botaoroyalsalute.bind('<Button-1>', lambda e: print('Opa'))
+
+## buchanans ==             ==                               ==              ==
+
+buchanans = Image.open("bebidalcolica\\buchananss.png")
+buchanans.thumbnail((300, 300))
+fotobuchanans = ImageTk.PhotoImage(buchanans)
+label_buchanans = tk.Label(telat3, image=fotobuchanans, bg="gray", bd=0)
+label_buchanans.place(rely=0.57, relx=0.35, anchor='n')
+
+
+botaobuchanans = Image.open("bebidalcolica\\botaobuchanans.png")
+botaobuchanans1 = ImageTk.PhotoImage(botaobuchanans)
+botaobuchanans = tk.Label(telat3, image=botaobuchanans1, bd=0)
+botaobuchanans.place(rely=0.79, relx=0.35, anchor="n")
+botaobuchanans.bind('<Button-1>', lambda e: print('Opa'))
+
+
+## black label      =               =             =             =              = 
+
+blacklabel = Image.open("bebidalcolica\\blacklabell.png")
+blacklabel.thumbnail((300, 300))
+fotoblacklabel = ImageTk.PhotoImage(blacklabel)
+label_blacklabel = tk.Label(telat3, image=fotoblacklabel, bg="gray", bd=0)
+label_blacklabel.place(rely=0.57, relx=0.65, anchor='n')
+
+botaoblacklabel = Image.open("bebidalcolica\\botaoblacklabel.png")
+botaoblacklabel1 = ImageTk.PhotoImage(botaoblacklabel)
+botaoblacklabel = tk.Label(telat3, image=botaoblacklabel1, bd=0)
+botaoblacklabel.place(rely=0.87, relx=0.65, anchor="n")
+botaoblacklabel.bind('<Button-1>', lambda e: print('Opa'))
+
+## TELA 4 ==            =               ==                ==               ==
+
+telat4 = tk.Frame(root, background='#ffff12')
+telat4.grid(column=0, row=0, sticky='nsew')
+
+titulot4 = tk.Label(telat4, text="Restaurante do Ederson", font=("Arial", 30, "bold"), anchor="center", background="yellow")
+titulot4.place(rely=0.00, relx=0.5, anchor='n')
+
 
 
 
