a4dcd728c0749c2ae87bd6165179bd941ab1c282
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-09 18:35:17 -0300

    dados inseridos
    inseri os dados da tabela do trabalho do professor ederson

--- banco_dados/cad.banco.sql
+++ banco_dados/cad.banco.sql
@@ -13,7 +13,7 @@ quem_indicou varchar(70)
 create table cadcligeral (
 id_cadcligeral int primary key auto_increment,
 aliquota float,
-tabela_preco float,
+tabela_preco enum ('', ''),
 transportadora1 varchar(40),
 transportadora2 varchar(40),
 regime_de_tributacao enum ('pj', 'pf'),
@@ -29,5 +29,29 @@ ramal varchar(20),
 tel_celular varchar(20) unique,
 fax varchar(20),
 cpf char(11) unique,
-data_nascm date
+insc_suframa varchar(20) unique,
+data_nascm date,
+mod_frete enum('', '', ''),
+st_de_cadastro enum ('', ''),
+rg varchar(15) unique,
+linha_do_perfil text ('', '')
+);
+
+create table sexo (
+IdSexo int primary key auto_increment,
+masculino char(1),
+feminino char(2),
+nao_se_aplica (3)
+);
+
+
+create table transportadora (
+IdTransp int primary key auto_increment,
+nome varchar(50)
+);
+
+create table endereco (
+IdEndereco int primary key auto_increment,
+
+
 );
