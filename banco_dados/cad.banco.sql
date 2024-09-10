06a4415888f3242a0ee6bcd41202faa0863d35c0
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-10 15:50:53 -0300

    o
    o

--- banco_dados/cad.banco.sql
+++ banco_dados/cad.banco.sql
@@ -13,7 +13,7 @@ quem_indicou varchar(70)
 create table cadcligeral (
 id_cadcligeral int primary key auto_increment,
 aliquota float,
-tabela_preco enum ('', ''),
+tabela_preco float,
 transportadora1 varchar(40),
 transportadora2 varchar(40),
 regime_de_tributacao enum ('pj', 'pf'),
@@ -38,18 +38,53 @@ linha_do_perfil text ('', '')
 );
 
 create table sexo (
-IdSexo int primary key auto_increment,
-tipo varcahr(20)
+idSexo int primary key auto_increment,
+tipo varchar(20)
 );
 
 
 create table transportadora (
-IdTransp int primary key auto_increment,
+idTransp int primary key auto_increment,
 nome varchar(50)
 );
 
 create table endereco (
-IdEndereco int primary key auto_increment,
+idEndereco int primary key auto_increment,
+rua varchar(50),
+cep varchar(8),
+bairro varchar(40),
+cidade varchar(40),
+numero int
+);
 
+create table perfil (
+idPerfil int primary key auto_increment,
+nome varchar(70),
+telefone varchar(20) unique,
+email varchar(50) unique,
+dataNasc date
+);
 
+create table comunicacao (
+idcomunicacao int primary key auto_increment,
+tipocontato varchar(50),
+valor varchar(100), 
+descricao text,
+rede social varchar(50)
+);
+
+create table vendedor (
+idvendedor int primary key auto_increment,
+nome varchar(70),
+datanasc date,
+email varchar(50) unique,
+salario decimal (10, 2)
+);
+
+create table financeiro (
+idfananceiro int primary key auto_increment,
+tipo_transacao varchar(50),
+categoria varchar(50),
+descricao text,
+data_transicao date,
 );
