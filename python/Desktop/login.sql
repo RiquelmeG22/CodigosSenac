0b9b1fb8a462dd32f727f55b6fd8012ecff0a8db
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-09-11 10:11:51 -0300

    oi
    oi

--- python/Desktop/login.sql
+++ python/Desktop/login.sql
@@ -2,16 +2,16 @@ create database login;
 
 use login;
 
-create table login (
-IdLogin int primary key auto_increment,
+create table cadastro (
+Idcadastro int primary key auto_increment,
 usuario varchar(50) unique,
 senha varchar(40),
 texto text
 );
 
-describe login; 
 
-insert into login (IdLogin, usuario, senha, texto) values
+
+insert into cadastro (Idcadastro, usuario, senha, texto) values
 ('1', 'Riq10', 'Senha231', 'GG');
 
-select * from login;
+select * from cadastro;
