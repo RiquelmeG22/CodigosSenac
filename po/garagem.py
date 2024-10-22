27462500b439a621c42f90cf2d6ee958889e27fd
RiquelmeG22 <riquelmeramalho762@gmail.com>
2024-10-22 11:11:29 -0400

    ok
    ok

---
+++ po/garagem.py
@@ -0,0 +1,30 @@
+class Veiculo:
+    def __init__(self, marca, modelo, cor, ano) -> None:
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+
+
+fusca = Veiculo("VolksWagen", "Fusca", "Azul", 1945)
+
+class Moto:
+    def __init__(self, marca, modelo, cor, ano) -> None:
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        
+        def acelerar(self):
+            print('Vraaaaaaaaaau')
+
+        def freiar(self):
+            print("RRRRaaannnnnn")
+
+class Carro(Veiculo):
+    def abrirPorta(self):
+        print("Abriu a porta")
+
+class Moto(Veiculo):
+    def empinar(self):
+        print("Bololololo")
