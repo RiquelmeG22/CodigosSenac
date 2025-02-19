eceb1adfd4be2ed0267bedf8978d80867d8d130a
RiquelmeG22 <riquelmeramalho762@gmail.com>
2025-02-19 18:54:11 -0400

    teste

--- python/Desktop/corum_prefeitos.py
+++ python/Desktop/corum_prefeitos.py
@@ -1,25 +1,25 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
-df = pd.read_csv('corum_prefeitos.csv')
+# df = pd.read_csv('corum_prefeitos.csv')
 
-plt.title('Vereadores Eleitos CG')
+# plt.title('Vereadores Eleitos CG')
 
-data = df.loc[:,['Nome', 'Voto']]
+# data = df.loc[:,['Nome', 'Voto']]
 
-print(len(data))
+# print(len(data))
 
-ep = [0.1 if i != 0 else 0.3 for i in range(len(data))]
+# ep = [0.1 if i != 0 else 0.3 for i in range(len(data))]
 
-plt.pie(data.iloc[:, 1], labels=data.iloc[:, 0], explode=ep,autopct='%.2f%%')
-plt.show()
+# plt.pie(data.iloc[:, 1], labels=data.iloc[:, 0], explode=ep,autopct='%.2f%%')
+# plt.show()
 
-plt.barh(data['Nome'], data['Voto'], color='blue')
+# plt.barh(data['Nome'], data['Voto'], color='blue')
 
-plt.xlabel('Voto')
-plt.ylabel('Vereadores')
-plt.tight_layout()
+# plt.xlabel('Voto')
+# plt.ylabel('Vereadores')
+# plt.tight_layout()
 
 
-plt.show()
+# plt.show()
 
